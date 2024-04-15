864462229f55863f8b79b795293ee7a7d20e9c94
Gabriel Orie <oriegabriel16@gmail.com>
2024-04-15 03:13:09 +0100

    Update & Finalize Group 1

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ const room = {
   width: 19.84,
   height: 4.91,
   occupants: 660,
-  ac_rating: 1.5,
+  ventilation_per_person: 0.3,
+  ac_rating: 3,
   air_changes: 1,
   infiltrated_air: 0,
   ac_count: 0,
@@ -15,12 +16,12 @@ const room = {
 
 const lighting = {
   count: 29,
-  allowance_factor: 1,
+  allowance_factor: 15,
   use_factor: 1.25,
 };
 
 const infiltrated_air = {
-  t1: 35,
+  t1: 41,
   t2: 23,
   w1: 0.00875,
   w2: 0.0159,
@@ -170,6 +171,7 @@ const SENSIBLE_HEAT_GAIN = {
     occupants: 0,
     infiltrated_air: 0,
     lighting: 0,
+    outside_air: 0,
   },
   per: {
     occupant: 65,
@@ -181,18 +183,19 @@ const LATENT_HEAT_GAIN = {
     occupants: 0,
     infiltrated_air: 0,
     lighting: 0,
+    outside_air: 0,
   },
   per: {
-    occupant: 65,
+    occupant: 55,
   },
 };
 
 const SOLAR_HEAT_GAIN_FACTOR = {
   windows: {
-    north: 0.14,
-    south: 0.14,
-    east: 0.14,
-    west: 0.14,
+    north: 128,
+    south: 20,
+    east: 151,
+    west: 21,
     total: 0,
   },
 };
@@ -371,10 +374,11 @@ function calculateWindowsSolarHeatGain() {
   let sections = ["north", "south", "east", "west"];
 
   sections.forEach((section) => {
-    SOLAR_HEAT_GAIN.windows[section] =
+    SOLAR_HEAT_GAIN.windows[section] = roundNumber(
       (windows.upper.area * windows.upper[section] +
         windows.lower.area * windows.lower[section]) *
-      SOLAR_HEAT_GAIN_FACTOR.windows[section];
+        SOLAR_HEAT_GAIN_FACTOR.windows[section]
+    );
   });
 
   SOLAR_HEAT_GAIN.windows.total = sumOfValues(SOLAR_HEAT_GAIN.windows, [
@@ -384,18 +388,20 @@ function calculateWindowsSolarHeatGain() {
 
 function calculateOccupantsParams() {
   SENSIBLE_HEAT_GAIN.due_to.occupants =
-    SENSIBLE_HEAT_GAIN.per.occupant * room.occupants;
+    (SENSIBLE_HEAT_GAIN.per.occupant * room.occupants) / 1000;
+
   LATENT_HEAT_GAIN.due_to.occupants =
-    LATENT_HEAT_GAIN.per.occupant * room.occupants;
+    (LATENT_HEAT_GAIN.per.occupant * room.occupants) / 1000;
 }
 
 function calculateOtherRequiredParams() {
   // CALCULATE SHG DUE TO LIGHTING
   SENSIBLE_HEAT_GAIN.due_to.lighting = roundNumber(
-    lighting.count *
-      (room.width * room.length) *
+    (room.width *
+      room.length *
       lighting.use_factor *
-      lighting.allowance_factor
+      lighting.allowance_factor) /
+      1000
   );
 
   // AMOUNT OF INF AIR
@@ -404,12 +410,30 @@ function calculateOtherRequiredParams() {
   );
 
   // SHG DUE TO INF AIR
-  SENSIBLE_HEAT_GAIN.due_to.infiltrated_air =
-    0.02044 * room.infiltrated_air * Math.abs(infiltrated_air.t1 - infiltrated_air.t2);
+  SENSIBLE_HEAT_GAIN.due_to.infiltrated_air = roundNumber(
+    0.02044 *
+      room.infiltrated_air *
+      Math.abs(infiltrated_air.t1 - infiltrated_air.t2)
+  );
 
   // LHG DUE TO INF AIR
-  LATENT_HEAT_GAIN.due_to.infiltrated_air =
-    50 * room.infiltrated_air * Math.abs(infiltrated_air.w1 - infiltrated_air.w2);
+  LATENT_HEAT_GAIN.due_to.infiltrated_air = roundNumber(
+    50 *
+      room.infiltrated_air *
+      Math.abs(infiltrated_air.w1 - infiltrated_air.w2)
+  );
+
+  let v1 = room.ventilation_per_person * room.occupants;
+  // SHG DUE TO OUT AIR
+  SENSIBLE_HEAT_GAIN.due_to.outside_air = roundNumber(
+    0.02044 * v1 * Math.abs(infiltrated_air.t1 - infiltrated_air.t2)
+  );
+
+  // LHG DUE TO OUT AIR
+  LATENT_HEAT_GAIN.due_to.outside_air = roundNumber(
+    50 * v1 * Math.abs(infiltrated_air.w1 - infiltrated_air.w2)
+  );
+  // console.warn(SENSIBLE_HEAT_GAIN.due_to);
 }
 
 function calculateTotalSensibleHeatGain() {
@@ -420,14 +444,20 @@ function calculateTotalSensibleHeatGain() {
 
 function doFinalCalculation() {
   let x = sumOfValues(SENSIBLE_HEAT_GAIN, ["due_to", "per"]) / 1000;
-  let y = sumOfValues(SENSIBLE_HEAT_GAIN.due_to) / 1000;
+  let y = sumOfValues(SENSIBLE_HEAT_GAIN.due_to);
 
-  let totalSHG = x + y;
-  let totalLHG = sumOfValues(LATENT_HEAT_GAIN) / 1000;
+  let totalSHG = x + y + SOLAR_HEAT_GAIN.windows.total / 1000;
+  let totalLHG = sumOfValues(LATENT_HEAT_GAIN.due_to);
 
-  console.warn((x + y) / room.ac_rating);
+  let totalHeat = (totalLHG + totalSHG) * 1000;
 
-  room.ac_count = Math.round((totalSHG + totalLHG) / room.ac_rating);
+  // CONVERT TO TONS THEN HP
+  let totalInTons = roundNumber(0.0002843 * totalHeat);
+  let totalInHP = 4.714 * totalInTons;
+
+  console.warn(totalInHP);
+
+  room.ac_count = Math.round(totalInHP / room.ac_rating);
 }
 
 // RECURSIVE FUNCTION TO SUM OBJECT VALUES WITH EXCLUSIONS
@@ -454,6 +484,7 @@ function updateAllValuesFromFields() {
   room.height = parseFloat($("#roomHeight").val()) || 0;
   room.occupants = parseFloat($("#occupantsCount").val()) || 0;
   room.ac_rating = parseFloat($("#roomRating").val()) || 0;
+  room.ventilation_per_person = parseFloat($("#vPerPerson").val()) || 0;
 
   // Update Lighting
   lighting.count = parseFloat($("#lightingCount").val()) || 0;
@@ -472,24 +503,39 @@ function updateAllValuesFromFields() {
   LATENT_HEAT_GAIN.per.occupant = parseFloat($("#occupantLHG").val()) || 0;
 
   // Update door values
-  doors.length = parseFloat($("#doorLength").val()) || 0;
-  doors.width = parseFloat($("#doorWidth").val()) || 0;
+  doors.big.length = parseFloat($("#doorLengthBig").val()) || 0;
+  doors.big.width = parseFloat($("#doorWidthBig").val()) || 0;
+  doors.small.length = parseFloat($("#doorLengthSmall").val()) || 0;
+  doors.small.width = parseFloat($("#doorWidthSmall").val()) || 0;
 
-  doors.north = parseInt($("#doorsCountNorth").val()) || 0;
-  doors.south = parseInt($("#doorsCountSouth").val()) || 0;
-  doors.east = parseInt($("#doorsCountEast").val()) || 0;
-  doors.west = parseInt($("#doorsCountWest").val()) || 0;
-  doors.area = doors.length * doors.width;
+  doors.big.north = parseInt($("#doorsCountNorthBig").val()) || 0;
+  doors.big.south = parseInt($("#doorsCountSouthBig").val()) || 0;
+  doors.big.east = parseInt($("#doorsCountEastBig").val()) || 0;
+  doors.big.west = parseInt($("#doorsCountWestBig").val()) || 0;
+  doors.big.area = doors.big.length * doors.big.width;
+  
+  doors.small.north = parseInt($("#doorsCountNorthSmall").val()) || 0;
+  doors.small.south = parseInt($("#doorsCountSouthSmall").val()) || 0;
+  doors.small.east = parseInt($("#doorsCountEastSmall").val()) || 0;
+  doors.small.west = parseInt($("#doorsCountWestSmall").val()) || 0;
+  doors.small.area = doors.small.length * doors.small.width;
 
   // Update window values
-  windows.length = parseFloat($("#windowsLength").val()) || 0;
-  windows.width = parseFloat($("#windowsWidth").val()) || 0;
+  windows.upper.length = parseFloat($("#windowsLengthUpper").val()) || 0;
+  windows.upper.width = parseFloat($("#windowsWidthUpper").val()) || 0;
 
-  windows.north = parseInt($("#windowsCountNorth").val()) || 0;
-  windows.south = parseInt($("#windowsCountSouth").val()) || 0;
-  windows.east = parseInt($("#windowsCountEast").val()) || 0;
-  windows.west = parseInt($("#windowsCountWest").val()) || 0;
-  windows.area = windows.length * windows.width;
+  windows.lower.length = parseFloat($("#windowsLengthLower").val()) || 0;
+  windows.lower.width = parseFloat($("#windowsWidthLower").val()) || 0;
+
+  windows.upper.north = parseInt($("#windowsCountNorthUpper").val()) || 0;
+  windows.upper.south = parseInt($("#windowsCountSouthUpper").val()) || 0;
+  windows.upper.east = parseInt($("#windowsCountEastUpper").val()) || 0;
+  windows.upper.west = parseInt($("#windowsCountWestUpper").val()) || 0;
+
+  windows.lower.north = parseInt($("#windowsCountNorthLower").val()) || 0;
+  windows.lower.south = parseInt($("#windowsCountSouthLower").val()) || 0;
+  windows.lower.east = parseInt($("#windowsCountEastLower").val()) || 0;
+  windows.lower.west = parseInt($("#windowsCountWestLower").val()) || 0;
 
   // Update Heat coefficient values
   HEAT_COEFFICIENT.walls.north =
@@ -549,10 +595,6 @@ function updateAllValuesFromFields() {
   EQUIVALENT_TEMPERATURE_DIFF.floor =
     parseFloat($("#eqTemperatureDifferenceFloor").val()) || 0;
 
-  // Log updated values
-  //   console.log(room);
-  //   console.log(doors);
-  //   console.log(windows);
 }
 
 function updateAllValuesToFields() {
@@ -562,6 +604,7 @@ function updateAllValuesToFields() {
   $("#roomHeight").val(room.height);
   $("#roomRating").val(room.ac_rating);
   $("#occupantsCount").val(room.occupants);
+  $("#vPerPerson").val(room.ventilation_per_person);
   $("#occupantSHG").val(SENSIBLE_HEAT_GAIN.per.occupant);
   $("#occupantLHG").val(LATENT_HEAT_GAIN.per.occupant);
 
@@ -577,24 +620,39 @@ function updateAllValuesToFields() {
   $("#infiltratedAirW2").val(infiltrated_air.w2);
 
   // Update door values
-  $("#doorLength").val(doors.length);
-  $("#doorWidth").val(doors.width);
+  $("#doorLengthBig").val(doors.big.length);
+  $("#doorWidthBig").val(doors.big.width);
+  $("#doorLengthSmall").val(doors.small.length);
+  $("#doorWidthSmall").val(doors.small.width);
 
-  $("#doorsCountNorth").val(doors.north);
-  $("#doorsCountSouth").val(doors.south);
-  $("#doorsCountEast").val(doors.east);
-  $("#doorsCountWest").val(doors.west);
-  doors.area = doors.length * doors.width;
+  $("#doorsCountNorthBig").val(doors.big.north);
+  $("#doorsCountSouthBig").val(doors.big.south);
+  $("#doorsCountEastBig").val(doors.big.east);
+  $("#doorsCountWestBig").val(doors.big.west);
+  // doors.area = doors.length * doors.width;
+  
+  $("#doorsCountNorthSmall").val(doors.small.north);
+  $("#doorsCountSouthSmall").val(doors.small.south);
+  $("#doorsCountEastSmall").val(doors.small.east);
+  $("#doorsCountWestSmall").val(doors.small.west);
+  // doors.area = doors.length * doors.width;
 
   // Update window values
-  $("#windowsLength").val(windows.length);
-  $("#windowsWidth").val(windows.width);
+  $("#windowsLengthUpper").val(windows.upper.length);
+  $("#windowsWidthUpper").val(windows.upper.width);
 
-  $("#windowsCountNorth").val(windows.north);
-  $("#windowsCountSouth").val(windows.south);
-  $("#windowsCountEast").val(windows.east);
-  $("#windowsCountWest").val(windows.west);
-  windows.area = windows.length * windows.width;
+  $("#windowsLengthLower").val(windows.lower.length);
+  $("#windowsWidthLower").val(windows.lower.width);
+
+  $("#windowsCountNorthUpper").val(windows.upper.north);
+  $("#windowsCountSouthUpper").val(windows.upper.south);
+  $("#windowsCountEastUpper").val(windows.upper.east);
+  $("#windowsCountWestUpper").val(windows.upper.west);
+  
+  $("#windowsCountNorthLower").val(windows.lower.north);
+  $("#windowsCountSouthLower").val(windows.lower.south);
+  $("#windowsCountEastLower").val(windows.lower.east);
+  $("#windowsCountWestLower").val(windows.lower.west);
 
   // Update Heat coefficient values
   $("#heatCoefficientNorthWall").val(HEAT_COEFFICIENT.walls.north);
@@ -722,6 +780,13 @@ function updateValuesInDisplay() {
   );
   $("#latentHeatGainDueToOccupants").html(LATENT_HEAT_GAIN.due_to.occupants);
   $("#sensibleHeatGainDueToLighting").html(SENSIBLE_HEAT_GAIN.due_to.lighting);
+  
+  $("#sensibleHeatGainDueToOutsideAir").html(
+    SENSIBLE_HEAT_GAIN.due_to.outside_air
+  );
+  $("#latentHeatGainDueToOutsideAir").html(
+    LATENT_HEAT_GAIN.due_to.outside_air
+  );
 
   $("#sensibleHeatGainDueToInfiltratedAir").html(
     SENSIBLE_HEAT_GAIN.due_to.infiltrated_air
